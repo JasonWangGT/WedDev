2e7ad39824107491060b5b970aa707bd4d634966
JasonWangGT <QiangW1218@outlook.com>
2017-04-24 13:13:44 -0400

    First commit

---
+++ js/myscripts.js
@@ -0,0 +1,43 @@
+//var firstname = "John";
+//var secondname = "Joe";
+//var active = true;
+//var age = 43;
+//var ageString = "43";
+//
+//if(age < 20) {
+//    console.log("John is young");
+//}
+//else if(age > 20 & age < 50) {
+//    console.log("John is middle age");
+//}
+//else {
+//    console.log("John is old");
+//}
+//
+//console.log(age == ageString);
+//console.log(age === ageString);
+//
+//function sayHello(name) {
+//    return "hello " + name;
+//}
+//
+//console.log(sayHello(firstname));
+
+//
+//var user = {
+//    firstname: "John",
+//    secondname: "Joe",
+//    active: true,
+//    age: 43,
+//    ageString: "43",
+//    
+//    sayHello: function() {
+//        return "hello " + this.firstname;
+//    }
+//};
+//
+//console.log(user.sayHello());
+
+$( document ).ready(function() {
+   console.log("Document loaded"); 
+});
